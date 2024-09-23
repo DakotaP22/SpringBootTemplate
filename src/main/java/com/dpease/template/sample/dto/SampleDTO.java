9374f2be81955e95fefb837ed196ebae2b4ecd6b
Dakota Pease <peased22@gmail.com>
2024-09-23 04:26:12 +0000

    refactor(sample): Adds constructors to SampleDTO

--- src/main/java/com/dpease/template/sample/dto/SampleDTO.java
+++ src/main/java/com/dpease/template/sample/dto/SampleDTO.java
@@ -1,8 +1,12 @@
 package com.dpease.template.sample.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SampleDTO {
     private String details;     
 }
